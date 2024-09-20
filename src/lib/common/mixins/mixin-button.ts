93fa25ec638010df50038eccacd12347f57c23f1
Willem Toerien <willem.toerien@wtprograms.com>
2024-09-21 00:03:11 +0400

    Added submitter

--- src/lib/common/mixins/mixin-button.ts
+++ src/lib/common/mixins/mixin-button.ts
@@ -1,13 +1,14 @@
 import { html, LitElement, nothing } from 'lit';
 import { property } from 'lit/decorators.js';
-import { FormSubmitterType } from '../behaviors/form-submission';
+import { FormSubmitterType, setupFormSubmitter } from '../behaviors/form-submission';
 import { MixinBase, MixinReturn } from './mixin';
 import { property$ } from '../lit/property$.decorator';
 import { Observable } from 'rxjs';
+import { internals, mixinElementInternals, WithElementInternals } from './mixin-internals';
 
 export type AnchorTarget = '' | '_blank' | '_self' | '_parent' | '_top';
 
-export interface Button {
+export interface Button extends WithElementInternals {
   type: FormSubmitterType;
   href: string | null;
   target: AnchorTarget;
@@ -20,7 +21,8 @@ export interface Button {
 export function mixinButton<T extends MixinBase<LitElement>>(
   base: T
 ): MixinReturn<T, Button> {
-  abstract class Mixin extends base implements Button {
+  const _base = mixinElementInternals(base);
+  abstract class Mixin extends _base implements Button {
     @property({ type: String })
     type: FormSubmitterType = 'button';
 
@@ -37,6 +39,12 @@ export function mixinButton<T extends MixinBase<LitElement>>(
     @property$()
     disabled = false;
     disabled$!: Observable<boolean>;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor(...args: any[]) {
+      super(args);
+      this.addEventListener('click', this.handleClick);
+    }
 
     renderAnchorOrButton(content: unknown = nothing) {
       return this.href
@@ -64,6 +72,47 @@ export function mixinButton<T extends MixinBase<LitElement>>(
         <div class="touch"></div>
         ${content}
       </button>`;
+    }
+
+    private async handleClick(event: Event) {
+      const {type, [internals]: elementInternals} = this;
+      const {form} = elementInternals;
+      if (!form || type === 'button') {
+        return;
+      }
+
+      // Wait a full task for event bubbling to complete.
+      await new Promise<void>((resolve) => {
+        setTimeout(resolve);
+      });
+
+      if (event.defaultPrevented) {
+        return;
+      }
+
+      if (type === 'reset') {
+        form.reset();
+        return;
+      }
+
+      // form.requestSubmit(submitter) does not work with form associated custom
+      // elements. This patches the dispatched submit event to add the correct
+      // `submitter`.
+      // See https://github.com/WICG/webcomponents/issues/814
+      form.addEventListener(
+        'submit',
+        (submitEvent) => {
+          Object.defineProperty(submitEvent, 'submitter', {
+            configurable: true,
+            enumerable: true,
+            get: () => this,
+          });
+        },
+        {capture: true, once: true},
+      );
+
+      elementInternals.setFormValue(this.value);
+      form.requestSubmit();
     }
   }
 
