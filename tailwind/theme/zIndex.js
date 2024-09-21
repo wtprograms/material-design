50ae9c07fd5df5ee572b1892a48118f19d499fa4
Willem Toerien <willem.toerien@wtprograms.com>
2024-09-21 19:49:10 +0400

    z-index fixes

--- tailwind/theme/zIndex.js
+++ tailwind/theme/zIndex.js
@@ -1,6 +1,6 @@
 module.exports = {
   'n1': 'var(--md-sys-z-index-n1)',
-  'app-bar': 'var(--md-sys-z-index-app-bar)',
-  'navigation-bar': 'var(--md-sys-z-index-navigation-bar)',
-  'scrim': 'var(--md-sys-z-index-scrim)',
+  'navigation': 'var(--md-sys-z-index-navigation)',
+  'top-bar': 'var(--md-sys-z-index-top-bar)',
+  'popover': 'var(--md-sys-z-index-popover)',
 };
