b594b1e1bd6fd38cf7b2ac3121f389d7ede925a3
Willem Toerien <willem.toerien@wtprograms.com>
2024-09-25 19:27:32 +0400

    added more properties for busy buttons.

--- src/lib/common/mixins/mixin-busy-button.ts
+++ src/lib/common/mixins/mixin-busy-button.ts
@@ -16,10 +16,31 @@ export function mixinBusyButton<T extends MixinBase<LitElement>>(
     @property({ type: Boolean, reflect: true })
     busy = false;
 
+    @property({ type: Number })
+    progress = 0;
+
+    @property({ type: Number })
+    max = 1;
+
+    @property({ type: Boolean })
+    indeterminate = false;
+
+    @property({ type: Boolean })
+    fourColor = false;
+
+    @property({ type: Number })
+    buffer = 0;
+
     renderProgressIndicator() {
       return this.busy && !this.disabled
         ? html`<div class="progress-indicator">
-            <md-progress-indicator indeterminate></md-progress-indicator>
+            <md-progress-indicator
+              ?indeterminate=${this.indeterminate}
+              value=${this.progress}
+              max=${this.max}
+              ?four-color=${this.fourColor}
+              buffer=${this.buffer}
+            ></md-progress-indicator>
           </div>`
         : nothing;
     }
