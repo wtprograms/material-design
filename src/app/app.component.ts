0ec110d3dc4de9897eab2927a948e028b1a67580
Willem Toerien <willem.toerien@wtprograms.com>
2024-10-26 18:09:04 +0400

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import routes from './routes';
-import { ListItemComponent } from '../../projects/wtprograms/material-design/src/lib/components/list-item/list-item.component';
-import { LinkDirective } from '../../projects/wtprograms/material-design/src/lib/directives/link.directive';
-import { NavigationComponent } from '../../projects/wtprograms/material-design/src/lib/components/navigation/navigation.component';
-import { NavigationItemComponent } from '../../projects/wtprograms/material-design/src/lib/components/navigation-item/navigation-item.component';
-import { SheetComponent } from '../../projects/wtprograms/material-design/src/lib/components/sheet/sheet.component';
+import {
+  ListItemComponent,
+  NavigationComponent,
+  NavigationItemComponent,
+  LinkDirective,
+  SheetComponent,
+} from '@wtprograms/material-design';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, ListItemComponent, NavigationComponent, NavigationItemComponent, LinkDirective, SheetComponent],
+  imports: [
+    RouterModule,
+    ListItemComponent,
+    NavigationComponent,
+    NavigationItemComponent,
+    LinkDirective,
+    SheetComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   host: {
-    class: 'tw flex gap-4 relative'
-  }
+    class: 'tw flex gap-4 relative',
+  },
 })
 export class AppComponent {
   readonly routes = routes.sort((a, b) => a.title.localeCompare(b.title));
