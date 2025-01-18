69d62e348b2b277822daebd5832c571a46aada6a
Willem Toerien <willem.toerien@wtprograms.com>
2025-01-18 22:55:04 +0400

    Fixes

--- projects/wtprograms/material-design/src/lib/components/list/list.component.ts
+++ projects/wtprograms/material-design/src/lib/components/list/list.component.ts
@@ -1,9 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  contentChildren,
+  effect,
+  input,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdComponent } from '../../common/base/md.component';
+import { MdListItemComponent } from '@wtprograms/material-design';
 
 @Component({
   selector: 'md-list',
@@ -12,6 +16,23 @@ import { MdComponent } from '../../common/base/md.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule],
 })
-export class MdListComponent extends MdComponent {}
+export class MdListComponent extends MdComponent {
+  readonly interactive = input<boolean>();
+  readonly listItems = contentChildren(MdListItemComponent);
+
+  constructor() {
+    super();
+    effect(() => {
+      const interactive = this.interactive();
+      if (interactive === undefined) {
+        return;
+      }
+      const listItems = this.listItems();
+      for (const listItem of listItems) {
+        listItem.interactive.set(interactive);
+      }
+    });
+  }
+}
 
 
--- projects/wtprograms/material-design/src/lib/components/popover/popover.component.ts
+++ projects/wtprograms/material-design/src/lib/components/popover/popover.component.ts
@@ -235,7 +235,6 @@ export class MdPopoverComponent extends MdComponent {
     } else {
       this._hostElement.style.width = '';
     }
-    const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
     const middleware: any[] = [
       offset(this.offset()),
       this.flip() ? flip() : undefined,
